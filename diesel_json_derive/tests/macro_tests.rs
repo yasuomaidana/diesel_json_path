c981abfe9fd6b562ee7db6b376480fd56b22cd96
yasuomaidana <yasuomaidana@gmail.com>
2026-04-09 18:04:15 -0600

    test: add additional macro generation tests for UserProfile2

--- diesel_json_derive/tests/macro_tests.rs
+++ diesel_json_derive/tests/macro_tests.rs
@@ -1,8 +1,8 @@
-use std::any::Any;
 use diesel::pg::Pg;
 use diesel::prelude::*;
 use diesel::sql_types::Integer;
 use diesel_json_derive::SqlFields;
+use std::any::Any;
 
 table! {
     users (id) {
@@ -21,20 +21,44 @@ struct UserProfile {
     theme: Option<String>,
 }
 
+#[derive(SqlFields)]
+struct UserProfile2 {
+    // defaults to body->>'id'::int
+    id: i32,
+    // metadata->'settings'->>'theme'
+    #[json_path("settings.theme")]
+    theme: Option<String>,
+}
+
+fn assert_integer_expr<E: Expression<SqlType = Integer>>(_e: &E) {}
 #[test]
 fn test_macro_generation() {
     let id_expr = UserProfile::id_sql();
     let theme_expr = UserProfile::theme_sql();
 
-    let id_sql =
-        diesel::debug_query::<Pg, _>(&users::table.select(id_expr)).to_string();
-    let theme_sql =
-        diesel::debug_query::<Pg, _>(&users::table.select(theme_expr)).to_string();
+    let id_sql = diesel::debug_query::<Pg, _>(&users::table.select(id_expr)).to_string();
+    let theme_sql = diesel::debug_query::<Pg, _>(&users::table.select(theme_expr)).to_string();
 
     assert_eq!(
         id_sql,
         "SELECT (metadata->>'id')::int FROM \"users\" -- binds: []"
     );
 
+    assert_integer_expr(&UserProfile::id_sql());
+
     assert!(theme_sql.contains("metadata->'settings'->>'theme'"));
+}
+
+#[test]
+fn test_macro_generation2() {
+    let id_expr = UserProfile2::id_sql();
+
+    let id_sql = diesel::debug_query::<Pg, _>(&users::table.select(id_expr)).to_string();
+
+    assert_eq!(
+        id_sql,
+        "SELECT (body->>'id')::int FROM \"users\" -- binds: []"
+    );
+
+    assert_integer_expr(&UserProfile2::id_sql());
 }
